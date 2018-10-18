649c70f9303df12d37d764743808cbe5c994fa28
VicChen1998 <895419327@qq.com>
2018-10-18 16:20:03 +0800

    0.15.1
    url匹配$
    选择题选项增加ABCD标示

--- server/QuestionBank/urls.py
+++ server/QuestionBank/urls.py
@@ -15,15 +15,15 @@ urlpatterns = [
 
     # 获取公共数据
     url(r'^public/get_college$', public.get_college),
-    url(r'^public/get_major', public.get_major),
-    url(r'^public/get_class', public.get_class),
-    url(r'^public/get_subject', public.get_subject),
+    url(r'^public/get_major$', public.get_major),
+    url(r'^public/get_class$', public.get_class),
+    url(r'^public/get_subject$', public.get_subject),
 
     # 获取个人数据
-    url(r'^personal/get_myupload', personal.get_myupload),
+    url(r'^personal/get_myupload$', personal.get_myupload),
 
     # 上传个人信息
-    url(r'^personal/set_userinfo', personal.set_userinfo),
+    url(r'^personal/set_userinfo$', personal.set_userinfo),
 
     # 提交题目
     url(r'^upload/choice$', upload.choice),
@@ -32,26 +32,26 @@ urlpatterns = [
     url(r'^upload/discuss$', upload.discuss),
 
     # 获取题目
-    url(r'^exercise/get_choice', exercise.get_choice),
-    url(r'^exercise/get_judge', exercise.get_judge),
-    url(r'^exercise/get_fill', exercise.get_fill),
-    url(r'^exercise/get_discuss', exercise.get_discuss),
+    url(r'^exercise/get_choice$', exercise.get_choice),
+    url(r'^exercise/get_judge$', exercise.get_judge),
+    url(r'^exercise/get_fill$', exercise.get_fill),
+    url(r'^exercise/get_discuss$', exercise.get_discuss),
 
     # 作业
     # 作业列表
-    url(r'^homework/list/class', homework.list_of_class),
-    url(r'^homework/list/teacher', homework.list_of_teacher),
+    url(r'^homework/list/class$', homework.list_of_class),
+    url(r'^homework/list/teacher$', homework.list_of_teacher),
     # 布置作业
     url(r'^homework/assign$', homework.assign),
     # 作业缴交情况
     url(r'^homework/detail$', homework.detail),
     # 查看学生答案
-    url(r'^homework/detail/by_student', homework.detail_by_student),
+    url(r'^homework/detail/by_student$', homework.detail_by_student),
     # 获取答案
-    url(r'homework/answer', homework.answer),
+    url(r'^homework/answer$', homework.answer),
     # 获取作业
-    url(r'^homework/get', homework.get),
+    url(r'^homework/get$', homework.get),
     # 上交
-    url(r'^homework/submit', homework.submit),
+    url(r'^homework/submit$', homework.submit),
 
 ]
